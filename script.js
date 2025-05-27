4685e78673aac6d88ddd8d5fde411408ef56444e
emilyborecki <163570215+emilyborecki@users.noreply.github.com>
2025-05-27 17:03:49 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.innerHTML = "🎊 A festa começou! Celebramos o trabalho no campo e a vida na cidade. Juntos somos mais fortes! 🌽🚶‍♂️🏡🏙️";
+}
